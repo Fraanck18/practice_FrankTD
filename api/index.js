e03d081a8d5c05664d947ddce921e55d12e1d211
Fraanck18 <andersonjaird05@gmail.com>
2026-04-16 22:07:40 +0000

    fix:solucionando errores archivo index

--- api/index.js
+++ api/index.js
@@ -1,15 +1,32 @@
 const http = require('http');
+const { Pool } = require('pg');
 
-const hostname = "0.0.0.0";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Bienvenidos al Servidor Node.js funcionando en Docker\n');
+// Configuración de la conexión (Debe coincidir con main.tf)
+const pool = new Pool({
+  user: 'admin_user',
+  host: 'bd',
+  database: 'practica_db',
+  password: 'password123',
+  port: 5432,
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Bienvenido http://${hostname}:${port}/`);
+const server = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  
+  try {
+    // Prueba de conexión simple
+    const result = await pool.query('SELECT NOW() as fechatiempo');
+    res.statusCode = 200;
+    res.end(JSON.stringify({ 
+      mensaje: "¡Conexión Exitosa con Postgres!", 
+      db_time: result.rows[0].fechatiempo 
+    }));
+  } catch (err) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ error: "Error de BD", detalles: err.message }));
+  }
 });
 
+server.listen(3000, '0.0.0.0', () => {
+  console.log('API lista en http://localhost:3000');
+});

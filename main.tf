2ac49ddfc4c843140548949358b7250e6e47d369
Fraanck18 <andersonjaird05@gmail.com>
2026-04-16 21:56:52 +0000

    feat: actualizacion

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "docker_container" "bd_cont" {
   image = docker_image.postgres_img.image_id
   networks_advanced { name = docker_network.app_network.name }
   
-  # Variables de entorno necesarias para que Postgres no se apague
+  # Variables para inicializar la BD y que no se apague el contenedor
   env = [
     "POSTGRES_DB=practica_db",
     "POSTGRES_USER=admin_user",
@@ -57,7 +57,7 @@ resource "docker_container" "bd_cont" {
   }
 }
 
-# Contenedor de la API
+# Contenedor de la API (Node.js/Python)
 resource "docker_container" "api_cont" {
   name  = "api01"
   image = docker_image.api_img.image_id
@@ -69,14 +69,14 @@ resource "docker_container" "api_cont" {
   }
 }
 
-# Contenedor Web
+# Contenedor Web (Node.js con index.js)
 resource "docker_container" "web_cont" {
   name  = "web-LOCAL-01"
   image = docker_image.web_img.image_id
   networks_advanced { name = docker_network.app_network.name }
   
   ports {
-    internal = 80
+    internal = 3000  # Cambiado de 80 a 3000 para coincidir con tu index.js
     external = 4001
   }
 }

c8d23c3ec756e19d9c76251a4528b13339acd7af
Fraanck18 <andersonjaird05@gmail.com>
2026-04-16 20:57:11 +0000

    fix: corrigiendo errores archivo main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+      version = "~> 2.25.0" 
     }
   }
 }
 
 provider "docker" {}
 
-# 1. Definición de la RED (Tal cual el diagrama: NETWORK)
+# 1. Definición de la RED (NETWORK)
 resource "docker_network" "app_network" {
   name = "NETWORK"
 }
@@ -21,7 +21,7 @@ resource "docker_image" "postgres_img" {
 
 resource "docker_container" "bd_cont" {
   name  = "bd"
-  image = docker_image.postgres_img.image_id
+  image = docker_image.postgres_img.latest
   networks_advanced {
     name = docker_network.app_network.name
   }
@@ -35,13 +35,13 @@ resource "docker_container" "bd_cont" {
 resource "docker_image" "api_img" {
   name = "api_image"
   build {
-    context = "./api"
+    path = "./api"
   }
 }
 
 resource "docker_container" "api_cont" {
   name  = "api01"
-  image = docker_image.api_img.image_id
+  image = docker_image.api_img.latest
   networks_advanced {
     name = docker_network.app_network.name
   }
@@ -55,13 +55,13 @@ resource "docker_container" "api_cont" {
 resource "docker_image" "web_img" {
   name = "web_image"
   build {
-    context = "./web"
+    path = "./web"
   }
 }
 
 resource "docker_container" "web_cont" {
   name  = "web-LOCAL-01"
-  image = docker_image.web_img.image_id
+  image = docker_image.web_img.latest
   networks_advanced {
     name = docker_network.app_network.name
   }
